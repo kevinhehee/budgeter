317a5c04c9e5cc6df580a45056eccd2dbfc15250
Kevin He <qweasd602og@gmail.com>
2026-03-08 17:47:09 -0700

    template budgets added

--- src/main/java/com/kevinhe/budgeter/service/BudgetService.java
+++ src/main/java/com/kevinhe/budgeter/service/BudgetService.java
@@ -10,6 +10,7 @@ import com.kevinhe.budgeter.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
